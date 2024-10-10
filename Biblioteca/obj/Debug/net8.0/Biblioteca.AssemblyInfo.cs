8215258c499514c39dd2edc1ad766dfbe74c4d69
Bruneizer <carlosbruno3856@gmail.com>
2024-10-10 10:47:57 -0300

    DER
    DER con sus datos y atributos

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1233081e97708f4ca53bb6df9ef77894b15aa24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9028ec7ad8b39d5c20a55b62915367a2ff714aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
